1b49cf17af7bb2425bc623d15a09b2ef3e0383de
Max Maischein <corion@corion.net>
2024-03-06 21:53:03 +0100

    Add documentation

--- lib/Future/Queue.pm
+++ lib/Future/Queue.pm
@@ -3,15 +3,50 @@ use 5.020;
 use Moo;
 use experimental 'signatures';
 
-has 'curr' => (
+=head1 NAME
+
+Future::Queue - a simple queue with a Future API
+
+=head1 SYNOPSIS
+
+  use Future::Utils 'repeat';
+  use Future::Queue;
+
+  my $q = Future::Queue->new();
+  
+  $q->enqueue('line 1');
+  $q->enqueue('line 2');
+  $q->enqueue('line 3');
+  $q->enqueue(undef); # our "end" marker
+  
+  my $curr = $q->head;
+  repeat {
+      my ($next,$str) = $curr->get;
+      say $str if defined $str;
+      $curr = $next;
+      
+      my $has_next = defined $str;
+      Future->done($has_next);
+  } while => sub($cont) { $cont->get };
+
+=cut
+
+has 'head' => (
     is => 'lazy',
     default => sub { Future->new },
 );
 
+has 'tail' => (
+    is => 'lazy',
+    default => sub { $_[0]->head },
+);
+
 sub enqueue($self, @stuff) {
-    my $curr = $self->curr;
-    $self->{curr} = Future->new;
-    return $curr->resolve( $self->curr, @stuff );
+    my $res = $self->tail;
+    $self->{tail} = Future->new;
+    return $curr->resolve( $self->tail, @stuff );
 }
 
 1;
+
+__END__
