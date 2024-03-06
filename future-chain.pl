0b8ad834c08d8706482f93d34476cf4c7c64f9be
Max Maischein <corion@corion.net>
2024-03-06 18:31:56 +0100

    Add Future::Queue testbed

---
+++ future-chain.pl
@@ -0,0 +1,31 @@
+use 5.020;
+use experimental 'signatures';
+use Future;
+use Future::Utils 'repeat';
+use Future::Queue;
+
+use Test2::V0 '-no_srand';
+
+my @lines = qw(
+    line1
+    line2
+    line3
+    line4
+);
+
+my $queue = Future::Queue->new;
+
+my $q = $queue->curr;
+for my $l (@lines) {
+    $queue->enqueue($l);
+}
+$queue->enqueue(undef);
+
+my $curr = $q;
+my %seen;
+repeat {
+    my ($next,$str) = $curr->get;
+    say $str if defined $str;
+    $curr = $next;
+    Future->done($str);
+} while => sub($cont) { defined $cont->get };
